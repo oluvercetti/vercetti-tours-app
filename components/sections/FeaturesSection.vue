a6d5c68163838bf4d02bbf3d3d03e92d7d501534
Ayodeji Idowu <ayawori25000@gmail.com>
2023-03-12 19:59:33 +0100

    created features section

---
+++ components/sections/FeaturesSection.vue
@@ -0,0 +1,64 @@
+<template>
+    <section class="section-features">
+        <div class="row">
+            <div class="col-1-of-4">
+                <div class="feature-box">
+                    <i class="feature-box__icon icon-basic-world" />
+                    <h3 class="heading-tertiary u-margin-bottom-small">
+                        Explore the world
+                    </h3>
+                    <p class="feature-box__text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto porro quidemiste ipsa
+                        voluptatum quos.
+                    </p>
+                </div>
+            </div>
+            <div class="col-1-of-4">
+                <div class="feature-box">
+                    <i class="feature-box__icon icon-basic-compass" />
+                    <h3 class="heading-tertiary u-margin-bottom-small">
+                        Meet Nature
+                    </h3>
+                    <p class="feature-box__text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto porro quidemiste ipsa
+                        voluptatum quos.
+                    </p>
+                </div>
+            </div>
+            <div class="col-1-of-4">
+                <div class="feature-box">
+                    <i class="feature-box__icon icon-basic-map" />
+                    <h3 class="heading-tertiary u-margin-bottom-small">
+                        Find Your Way
+                    </h3>
+                    <p class="feature-box__text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto porro quidemiste ipsa
+                        voluptatum quos.
+                    </p>
+                </div>
+            </div>
+            <div class="col-1-of-4">
+                <div class="feature-box">
+                    <i class="feature-box__icon icon-basic-heart" />
+                    <h3 class="heading-tertiary u-margin-bottom-small">
+                        Live a healthier life
+                    </h3>
+                    <p class="feature-box__text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto porro quidemiste ipsa
+                        voluptatum quos.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
