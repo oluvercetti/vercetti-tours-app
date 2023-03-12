4fbd8753601ca8a1cd3560cf42482318f056f5e0
Ayodeji Idowu <ayawori25000@gmail.com>
2023-03-12 19:59:15 +0100

    created about section

---
+++ components/sections/AboutSection.vue
@@ -0,0 +1,66 @@
+<template>
+    <section class="section-about">
+        <div class="u-center-text u-margin-bottom-big">
+            <h2 class="heading-secondary">
+                Exciting tours for adventurous people
+            </h2>
+        </div>
+
+        <div class="row">
+            <div class="col-1-of-2">
+                <h3 class="heading-tertiary u-margin-bottom-small">
+                    You're going to fall in love with nature
+                </h3>
+                <p class="paragraph">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Dolorum quaerat sunt mollitia incidunt excepturi ea ab
+                    modi voluptas! Suscipit tempora magnam id maiores
+                    consectetur quos molestias tenetur culpa laboriosam accusamus.
+                </p>
+                <h3 class="heading-tertiary u-margin-bottom-small">
+                    Live adventures like you have never before
+                </h3>
+                <p class="paragraph">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Assumenda delectus dolorem enim minus.
+                </p>
+                <a href="#" class="btn-text">Learn more &rarr;</a>
+            </div>
+            <div class="col-1-of-2">
+                <div class="composition">
+                    <img
+                        srcset="img/nat-1.jpg 300w, img/nat-1-large.jpg 1000w"
+                        sizes="(max-width: 56.25em) 20w, (max-width: 37.5em) 30vw"
+                        src="img/nat-1-large.jpg"
+                        alt="Photo 1"
+                        class="composition__photo composition__photo--p1"
+                    >
+                    <img
+                        srcset="img/nat-2.jpg 300w, img/nat-2-large.jpg 1000w"
+                        sizes="(max-width: 56.25em) 20w, (max-width: 37.5em) 30vw"
+                        src="img/nat-2-large.jpg"
+                        alt="Photo 2"
+                        class="composition__photo composition__photo--p2"
+                    >
+                    <img
+                        srcset="img/nat-3.jpg 300w, img/nat-3-large.jpg 1000w"
+                        sizes="(max-width: 56.25em) 20w, (max-width: 37.5em) 30vw"
+                        src="img/nat-3-large.jpg"
+                        alt="Photo 3"
+                        class="composition__photo composition__photo--p3"
+                    >
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
