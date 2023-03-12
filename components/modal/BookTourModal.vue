48226955af6a601686c6f2e96ee87c32d0e1ce1a
Ayodeji Idowu <ayawori25000@gmail.com>
2023-03-12 19:56:48 +0100

    created book tour modal

---
+++ components/modal/BookTourModal.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="popup" class="popup">
+        <div class="popup__content">
+            <div class="popup__left">
+                <img src="img/nat-8.jpg" alt="Tour photo" class="popup__img">
+                <img src="img/nat-9.jpg" alt="Tour photo" class="popup__img">
+            </div>
+            <div class="popup__right">
+                <a href="#section-tours" class="popup__close">&times;</a>
+                <h2 class="heading-secondary u-margin-bottom-small">
+                    Start booking now
+                </h2>
+                <h3 class="heading-tertiary u-margin-bottom-small">
+                    Important &ndash; Please read these terms before
+                    booking
+                </h3>
+                <p class="popup__text">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt cupiditate sit, qui autem rerum
+                    officiis quas suscipit tempore labore explicabo vel doloribus. Eligendi saepe incidunt libero
+                    officia doloribus exercitationem at!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. A similique magni fuga ipsam minima est
+                    illum rem nam sapiente architecto nostrum esse dolores, recusandae, atque cumque repellendus
+                    dignissimos distinctio nemo.
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde ducimus dolores labore vitae numquam
+                    in, accusamus magnam soluta aperiam tempora enim, aut eveniet accusantium sed sit officia officiis,
+                    molestias id.
+                </p>
+                <a href="#" class="btn btn--green">Book Now</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
